ad1e5cd818859245f4f7f01e870fd32e87b4c657
israel <israel.aires@sequor.com.br>
2021-07-11 19:23:55 -0300

    feat: layout improvements
    * Replace checkbox by toggle button
    * Switch Consumer Unit and Supplier position on views

--- Models/PowerPlant.cs
+++ Models/PowerPlant.cs
@@ -1,13 +1,22 @@
-using System;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace crud_registration_web_system.Models
 {
     public class PowerPlant
     {
         public int Id { get; set; }
-        public string ConsumerUnit { get; set; }
+
+        [Display(Name = "Fornecedor")]
         public string Supplier { get; set; }
+        // public IEnumerable<SelectListItem> Suppliers { get; set; }
+        
+        [Display(Name = "UC da Usina")]
+        public string ConsumerUnit { get; set; }
+        
+
+        [Display(Name = "Ativo")]
         public bool Active{get;set;} = true;
    }
 }
